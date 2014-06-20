17b675b8ddedfa4cd73dfd168aede81f49be878f
Jay Peretz <jay@mytowncontractor.com>
2014-06-20 09:34:12 -0700

    remove db connector

--- lesson-maker/sql-jperetz.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/* create a SQL connection */
-$mysqli = new mysqli("student.santarosa.edu", "jperetz_admin", "0CniBVJG", "jperetz");
-
-/* check SQL connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
